cb464d4cd83c8cdfe1955969a2e1d76526e3a4cb
Noah Silva <acv7qc@virginia.edu>
2024-04-14 16:16:46 +0000

    Reformatted comments to document sources used

--- lab8/extract_json.py
+++ lab8/extract_json.py
@@ -12,6 +12,8 @@ with open('lab8/chacon.csv', 'w', newline='') as file_csv:
         chacon_newline = [data[d]['name'],data[d]['html_url'],data[d]['updated_at'],data[d]['visibility']]
         writer.writerow(chacon_newline)
 
+# Sources:
+
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/
 # https://stackoverflow.com/questions/1938227/int-object-is-not-iterable-while-trying-to-sum-the-digits-of-a-number
 # https://stackoverflow.com/questions/58192471/csv-writer-inserting-comma-between-each-character
